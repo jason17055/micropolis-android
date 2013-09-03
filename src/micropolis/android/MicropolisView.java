3956590f670d3f78990c7c643e2afad18c7ba948
Jason Long <jason@long.name>
2013-09-02 20:40:27 -0400

    resource: try adding a bitmap resource

--- src/micropolis/android/MicropolisView.java
+++ src/micropolis/android/MicropolisView.java
@@ -2,12 +2,14 @@ package micropolis.android;
 
 import android.content.Context;
 import android.graphics.*;
+import android.graphics.drawable.*;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class MicropolisView extends View
 {
 	Paint piePaint;
+	Bitmap tilesBitmap;
 	
 	public MicropolisView(Context context, AttributeSet attrs)
 	{
@@ -16,8 +18,21 @@ public class MicropolisView extends View
 		piePaint = new Paint();
 		piePaint.setColor(0xffff0000);
 		piePaint.setStyle(Paint.Style.FILL);
+		
+		loadTilesBitmap();
 	}
-	
+
+	private void loadTilesBitmap()
+	{
+		Drawable d = getResources().getDrawable(R.drawable.tiles);
+		this.tilesBitmap = ((BitmapDrawable) d).getBitmap();
+/*		BitmapFactory.Options options = new BitmapFactory.Options();
+		this.tilesBitmap = BitmapFactory.decodeResource(
+					getResources(),
+					"res/graphics/tiles.png",
+					options);*/
+	}
+
 	@Override
 	public void onDraw(Canvas canvas)
 	{
