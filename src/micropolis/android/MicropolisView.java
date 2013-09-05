f5876c9f3ec8ecfee311f7d30c235cf124525aaf
Jason Long <jlong@messiah.edu>
2013-09-05 10:53:23 -0400

    view: respond to scroll gesture

--- src/micropolis/android/MicropolisView.java
+++ src/micropolis/android/MicropolisView.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.graphics.*;
 import android.graphics.drawable.*;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.*;
 
 public class MicropolisView extends View
 {
@@ -42,21 +42,66 @@ public class MicropolisView extends View
 				options);
 	}
 
+	int tileSize = 32;
+	float originX = 0.0f;
+	float originY = 0.0f;
+
 	@Override
 	public void onDraw(Canvas canvas)
 	{
 		Paint p = new Paint();
 
-		for (int y = 0; y < 32; y++) {
-			for (int x = 0; x < 32; x++) {
+		canvas.save();
+		canvas.translate(-originX, -originY);
+
+		Rect bounds = canvas.getClipBounds();
+		int minY = bounds.top / tileSize;
+		int maxY = bounds.bottom / tileSize + 1;
+		int minX = bounds.left / tileSize;
+		int maxX = bounds.right / tileSize + 1;
+
+		minY = Math.max(minY, 0);
+		maxY = Math.min(maxY, city.getHeight());
+		minX = Math.max(minX, 0);
+		maxX = Math.min(maxX, city.getWidth());
+
+		for (int y = minY; y < maxY; y++) {
+			for (int x = minX; x < maxX; x++) {
 				int t = city.getTile(x, y) & TileConstants.LOMASK;
 				t = t % 64;
 
 				canvas.drawBitmap(tilesBitmap,
-					new Rect(0, t*32, 32, t*32+32),
-					new Rect(x*32, y*32, x*32+32, y*32+32),
+					new Rect(0, t*tileSize, tileSize, t*tileSize+tileSize),
+					new Rect(x*tileSize, y*tileSize, x*tileSize+tileSize, y*tileSize+tileSize),
 					p);
 			}
 		}
+
+		canvas.restore();
+	}
+
+	class MyGestureListener extends GestureDetector.SimpleOnGestureListener
+	{
+		@Override
+		public boolean onDown(MotionEvent ev)
+		{
+			return true;
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+		{
+			originX += distanceX;
+			originY += distanceY;
+			invalidate();
+			return true;
+		}
+	}
+	GestureDetector gestDetector = new GestureDetector(getContext(), new MyGestureListener());
+
+	@Override
+	public boolean onTouchEvent(MotionEvent evt)
+	{
+		return gestDetector.onTouchEvent(evt);
 	}
 }
