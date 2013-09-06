f4656c55e69bdc17fa7d9d8927f6e5caac78bb75
Jason Long <jlong@messiah.edu>
2013-09-06 13:42:27 -0400

    tools: add several Micropolis tool buttons

--- src/micropolis/android/MainActivity.java
+++ src/micropolis/android/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import micropolisj.engine.*;
 
@@ -35,9 +36,72 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	void doRoadTool()
+	void setTool(MicropolisTool newTool)
 	{
 		MicropolisView view = (MicropolisView) findViewById(R.id.main_view);
-		view.setTool(MicropolisTool.ROADS);
+		view.setTool(newTool);
+	}
+
+	void doRoadTool()
+	{
+	}
+
+	private void setToolsVisibility(int v)
+	{
+		findViewById(R.id.road_tool_btn).setVisibility(v);
+		findViewById(R.id.rail_tool_btn).setVisibility(v);
+		findViewById(R.id.wire_tool_btn).setVisibility(v);
+		findViewById(R.id.res_tool_btn).setVisibility(v);
+		findViewById(R.id.com_tool_btn).setVisibility(v);
+		findViewById(R.id.ind_tool_btn).setVisibility(v);
+	}
+
+	public void onRoadToolClicked(View view)
+	{
+		setToolsVisibility(View.GONE);
+		findViewById(R.id.ok_btn).setVisibility(View.VISIBLE);
+		setTool(MicropolisTool.ROADS);
+	}
+
+	public void onRailToolClicked(View view)
+	{
+		setToolsVisibility(View.GONE);
+		findViewById(R.id.ok_btn).setVisibility(View.VISIBLE);
+		setTool(MicropolisTool.RAIL);
+	}
+
+	public void onWireToolClicked(View view)
+	{
+		setToolsVisibility(View.GONE);
+		findViewById(R.id.ok_btn).setVisibility(View.VISIBLE);
+		setTool(MicropolisTool.WIRE);
+	}
+
+	public void onResZoneToolClicked(View view)
+	{
+		setToolsVisibility(View.GONE);
+		findViewById(R.id.ok_btn).setVisibility(View.VISIBLE);
+		setTool(MicropolisTool.RESIDENTIAL);
+	}
+
+	public void onComZoneToolClicked(View view)
+	{
+		setToolsVisibility(View.GONE);
+		findViewById(R.id.ok_btn).setVisibility(View.VISIBLE);
+		setTool(MicropolisTool.COMMERCIAL);
+	}
+
+	public void onIndZoneToolClicked(View view)
+	{
+		setToolsVisibility(View.GONE);
+		findViewById(R.id.ok_btn).setVisibility(View.VISIBLE);
+		setTool(MicropolisTool.INDUSTRIAL);
+	}
+
+	public void onOkClicked(View view)
+	{
+		findViewById(R.id.ok_btn).setVisibility(View.GONE);
+		setToolsVisibility(View.VISIBLE);
+		setTool(null);
 	}
 }
