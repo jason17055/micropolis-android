e50715166c064013cf082483014d72f526f739af
Jason Long <jlong@messiah.edu>
2013-09-09 14:21:38 -0400

    implement a screen for "start new city"

---
+++ src/micropolis/android/NewCityActivity.java
@@ -0,0 +1,25 @@
+package micropolis.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class NewCityActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_city_activity);
+
+		Spinner levelSpinner = (Spinner)
+			findViewById(R.id.level_spinner);
+		ArrayAdapter<CharSequence> adapter =
+			ArrayAdapter.createFromResource(this,
+				R.array.levels_array,
+				android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		levelSpinner.setAdapter(adapter);
+	}
+}
--- src/micropolis/android/StartActivity.java
+++ src/micropolis/android/StartActivity.java
@@ -1,7 +1,9 @@
 package micropolis.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends Activity
 {
@@ -10,5 +12,12 @@ public class StartActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_activity);
+	}
+
+	/** Called when the user clicks the Start New City button. */
+	public void startNewCity(View btn)
+	{
+		Intent intent = new Intent(this, NewCityActivity.class);
+		startActivity(intent);
 	}
 }
