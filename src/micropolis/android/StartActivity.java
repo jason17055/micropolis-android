94e5765288051f1a276973e3e98612fb0676bedc
Jason Long <jlong@messiah.edu>
2013-09-09 13:51:48 -0400

    implement a new Start screen

---
+++ src/micropolis/android/StartActivity.java
@@ -0,0 +1,14 @@
+package micropolis.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start_activity);
+	}
+}
